9afbb903a263d2a8e0b2f65468249d04734778f5
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-03-24 09:13:23 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { CityPicker } from "@/components/city-picker";
 
 export default function Home() {
   return (
-   <main className="flex min-h-screen flex-col items-center p-24 bg-gradient-to-b from-green-900/90 via-green-400/90 to-green-200/90">
+   <main className="flex min-h-screen flex-col items-center p-24 bg-gradient-to-b from-grey-100/90 via-grey-100/90 to-blue-300/90">
     <CityPicker/>
    </main>
   );
--- src/components/city-picker.tsx
+++ src/components/city-picker.tsx
@@ -1,40 +1,66 @@
-import { Country,State,City } from "country-state-city"
+import { Country, State, City } from "country-state-city"
 
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, } from "@/components/ui/select"
 import { MapPinIcon } from "lucide-react"
+import { Button } from "./ui/button"
 
-export  function CityPicker() {
+export function CityPicker() {
   return (
-    <Card className="bg-green-200/40 border-none w-full md:w-96 flex justify-center">
-    <CardHeader className="flex justify-center">
-      <CardTitle >
-        <div className="flex justify-center text-white">
-            <MapPinIcon className="size-6 mr-2"/>
+    <Card className="bg-grey-100/40 border-none w-full md:w-96 flex justify-center">
+      
+      <CardHeader className="flex justify-center">
+        <CardTitle >
+          <div className="flex justify-center ">
+            <MapPinIcon className="size-6 mr-2" />
             <h2>Select City</h2>
+          </div>
+        </CardTitle>
+      </CardHeader>
+      
+      <CardContent>
+        <div className="space-y-4">
+          <Select>
+            <SelectTrigger className="w-full" >
+              <SelectValue placeholder="Select a City" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select a City" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select a City" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-      </CardTitle>
-     
-    </CardHeader>
-    <CardContent>
-      <p>Card Content</p>
-    </CardContent>
-    <CardFooter>
-      <p>Card Footer</p>
-    </CardFooter>
-  </Card>
+      </CardContent>
+
+
+      <CardFooter className="flex justify-end">
+        <Button className="bg-green-500 hover:bg-green-500/90">Find</Button>
+      </CardFooter >
+
+
+    </Card>
+
+
   )
 }
