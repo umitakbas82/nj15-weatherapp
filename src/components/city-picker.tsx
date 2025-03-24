f64963c14790cf416005b516269d9348b87c55db
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-03-24 14:09:59 +0300

    commit

--- src/components/city-picker.tsx
+++ src/components/city-picker.tsx
@@ -1,13 +1,59 @@
+"use client";
 import { Country, State, City } from "country-state-city"
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, } from "@/components/ui/select"
 import { MapPinIcon } from "lucide-react"
 import { Button } from "./ui/button"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+type TCountry={
+  value:{
+    latitude:string;
+    longitude:string;
+    isoCode:string;
+  };
+  label:string;
+}|null;
+
+type TState={
+  value:{
+    latitude:string;
+    longitude:string;
+    countryCode:string;
+    name:string;
+    isoCode:string;    
+  }; label:string;
+}|null;
+type TCity={
+  value:{
+    latitude:string;
+    longitude:string;
+    countryCode:string;
+    name:string;
+    stateCode:string;    
+  }; label:string;
+}|null;
+
+const countries = Country.getAllCountries().map((country)=>({
+  value:{
+    latitude:country.latitude,
+    longitude:country.longitude,
+    isoCode:country.isoCode
+  },
+  label:country.name
+}))
 
 export function CityPicker() {
+const[selectCountry,setSelectCountry]=useState<TCountry>(null);
+const[selectState,setSelectState]=useState<TState>(null)
+const[selectCity,setSelectCity]=useState<TCity>(null)
+
   return (
-    <Card className="bg-grey-100/40 border-none w-full md:w-96 flex justify-center">
+    
+    <Card className="bg-grey-100/40 border-none w-full md:w-150 flex justify-center">
       
       <CardHeader className="flex justify-center">
         <CardTitle >
@@ -60,7 +106,7 @@ export function CityPicker() {
 
 
     </Card>
-
+    
 
   )
 }
